13ddd1a8f82fe12a8b778610593af72d97f7cd12
katerinamant <katerinamantaraki@gmail.com>
2024-05-02 19:40:12 +0300

    add font + add landing page activity layout

---
+++ app/src/main/java/com/example/lapis/LandingPage/LandingPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.lapis.LandingPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lapis.R;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
