7af8714bc7a638020d85fe981d3289205e8441e5
katerinamant <katerinamantaraki@gmail.com>
2024-05-03 17:18:40 +0300

    add functionality to LandingPageActivity login button with new LoginActivity + create LoginActivity layout

--- app/src/main/java/com/example/lapis/LandingPage/LandingPageActivity.java
+++ app/src/main/java/com/example/lapis/LandingPage/LandingPageActivity.java
@@ -2,8 +2,12 @@ package com.example.lapis.LandingPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.lapis.LoginPage.LoginActivity;
 import com.example.lapis.R;
 
 public class LandingPageActivity extends AppCompatActivity {
@@ -12,5 +16,17 @@ public class LandingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Button getStartedButton = findViewById(R.id.btn_get_started);
+        // TODO: Add functionality
+
+        Button loginButton = findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingPageActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
