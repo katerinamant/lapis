08c091185eab4f84999401871093b70751cb80a2
katerinamant <katerinamantaraki@gmail.com>
2024-05-07 16:13:48 +0300

    add RentalPageActivity design

---
+++ app/src/main/java/com/example/lapis/RentalPage/RentalPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.lapis.RentalPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lapis.R;
+
+public class RentalPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rental_page);
+    }
+}
