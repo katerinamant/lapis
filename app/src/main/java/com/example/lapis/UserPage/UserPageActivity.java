03777b9b3e6faaa6f53530c37e57081d431e1a1a
katerinamant <katerinamantaraki@gmail.com>
2024-05-15 20:47:42 +0300

    reformat code

--- app/src/main/java/com/example/lapis/UserPage/UserPageActivity.java
+++ app/src/main/java/com/example/lapis/UserPage/UserPageActivity.java
@@ -1,10 +1,10 @@
 package com.example.lapis.UserPage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lapis.HomePage.HomePageActivity;
 import com.example.lapis.R;
