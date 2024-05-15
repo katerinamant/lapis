3f46d6850d085aebf519f3cb92e7ead506d8ef90
katerinamant <katerinamantaraki@gmail.com>
2024-05-15 21:23:38 +0300

    add SearchPageActivity + transition from HomePageActivity

--- app/src/main/java/com/example/lapis/HomePage/HomePageActivity.java
+++ app/src/main/java/com/example/lapis/HomePage/HomePageActivity.java
@@ -2,11 +2,14 @@ package com.example.lapis.HomePage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lapis.R;
+import com.example.lapis.SearchPage.SearchPageActivity;
 import com.example.lapis.UserPage.UserPageActivity;
 
 public class HomePageActivity extends AppCompatActivity {
@@ -18,11 +21,24 @@ public class HomePageActivity extends AppCompatActivity {
 
         // User button
         ImageView headerLogo = findViewById(R.id.header_user);
-        headerLogo.setOnClickListener(view -> goToUserPage());
-    }
+        headerLogo.setOnClickListener(view -> {
+            // Go to UserPage
+            Intent intent = new Intent(HomePageActivity.this, UserPageActivity.class);
+            startActivity(intent);
+        });
 
-    private void goToUserPage() {
-        Intent intent = new Intent(HomePageActivity.this, UserPageActivity.class);
-        startActivity(intent);
+        EditText searchbar = findViewById(R.id.edit_text_destination);
+        searchbar.setOnEditorActionListener((textView, i, keyEvent) -> {
+            if (i == EditorInfo.IME_ACTION_SEARCH) {
+                Intent intent = new Intent(HomePageActivity.this, SearchPageActivity.class);
+
+                // Add destination input to intent
+                String destination = searchbar.getText().toString().trim();
+                intent.putExtra("destination", destination);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
     }
 }
---
+++ app/src/main/java/com/example/lapis/SearchPage/SearchPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.lapis.SearchPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lapis.R;
+
+public class SearchPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_page);
+    }
+}
